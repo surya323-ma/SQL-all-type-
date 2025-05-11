341b7e7568da6771f39cda7e3ab3e372bfe23607
surya omar <suryaomar323@gmail.com>
2025-05-11 15:58:14 +0530

    Create All products.sql

---
+++ All products.sql
@@ -0,0 +1,19 @@
+All Products
+Task
+Write a query to output all the columns from the Products table.
+
+Table name: Products
+
+product_id	product_name	price	category
+1	Laptop	999.99	Electronics
+2	Desk Chair	149.99	Furniture
+3	Smartphone	599.99	Electronics
+4	Notebook	2.99	Stationery
+5	Headphones	89.99	Electronics
+6	Coffee Maker	49.99	Appliances
+7	Standing Desk	249.99	Furniture
+8	Tablet	399.99	Electronics
+9	Mouse	19.99	Electronics
+10	Water Bottle	12.99	Stationery
+SQL
+select* from products;
