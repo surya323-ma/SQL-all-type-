1de041c3d1a1df2b77fd412b8475f9fedabdf25a
surya omar <suryaomar323@gmail.com>
2025-05-23 21:40:47 +0530

    Update 1934. Confirmation Rate.py

--- 1934. Confirmation Rate.py
+++ 1934. Confirmation Rate.py
@@ -25,7 +25,7 @@ Each row of this table indicates that the user with ID user_id requested a confi
 The confirmation rate of a user is the number of 'confirmed' messages divided by the total number of requested confirmation messages. The confirmation rate of a user that did not request any confirmation messages is 0. Round the confirmation rate to two decimal places.
 Write a solution to find the confirmation rate of each user.
 Return the result table in any order.
-
+#CODE HERE
 Select s.user_id, round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate from Signups as s left join Confirmations as c on s.user_id= c.user_id group by user_id;                                                   
                                                    
 
