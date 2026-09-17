48e051f276d526559918e169b68fd2883dfdb3f1
surya omar <suryaomar323@gmail.com>
2026-09-18 00:22:37 +0530

    Create 177. Nth Highest Salary.sql

---
+++ 177. Nth Highest Salary.sql
@@ -0,0 +1,24 @@
+Table: Employee
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+
+Write a solution to find the nth highest distinct salary from the Employee table. If there are less than n distinct salaries, return null.
+The result format is in the following example.
+
+ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    SET N = N - 1;
+
+    RETURN (
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT N, 1
+    );
+END
